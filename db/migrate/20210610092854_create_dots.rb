057a01d245f36cd8eac79b537f2f01154f1e51cd
xiangma0531 <shoma0531@outlook.com>
2021-06-10 18:36:32 +0900

    dotsテーブル作成

--- db/migrate/20210610092854_create_dots.rb
+++ db/migrate/20210610092854_create_dots.rb
@@ -1,7 +1,10 @@
 class CreateDots < ActiveRecord::Migration[6.0]
   def change
     create_table :dots do |t|
-
+      t.string :title, null: false
+      t.integer :category_id, null: false
+      t.text :content, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_084924) do
+ActiveRecord::Schema.define(version: 2021_06_10_092854) do
+
+  create_table "dots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "category_id", null: false
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dots_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_06_10_084924) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dots", "users"
 end
