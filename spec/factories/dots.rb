793d15f9924c2f2fd51a948778bbb6407832f085
xiangma0531 <shoma0531@outlook.com>
2021-06-11 12:11:03 +0900

    Dotモデル単体テストコード実装

--- spec/factories/dots.rb
+++ spec/factories/dots.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :dot do
-    
+    title { "テスト" }
+    category_id { 1 }
+    content { Faker::Lorem.sentence }
+    association :user
   end
 end
--- spec/models/dot_spec.rb
+++ spec/models/dot_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Dot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @dot = FactoryBot.build(:dot)
+    end
+
+    context '新規投稿できるとき' do
+      it 'すべての項目が正しく入力できていれば新規投稿できる' do
+        expect(@dot).to be_valid
+      end
+    end
+
+    context '新規投稿できないとき' do
+      it 'titleが空では新規投稿できない' do
+        @dot.title = ''
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'categoryが空では新規登録できない' do
+        @dot.category_id = nil
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'contentが空では新規投稿できない' do
+        @dot.content = ''
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("Content can't be blank")
+      end
+
+      it 'userが紐づいていないと新規投稿できない' do
+        @dot.user = nil
+        @dot.valid?
+        expect(@dot.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
