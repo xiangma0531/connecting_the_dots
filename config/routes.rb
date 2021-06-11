bea52a6b9b4bd44a25bb51091d032a3bada269e0
xiangma0531 <shoma0531@outlook.com>
2021-06-11 12:45:22 +0900

    投稿編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
-  resources :dots, only: [:new, :create, :show]
+  resources :dots, only: [:new, :create, :show, :edit, :update]
 end
